c726f09a54ff97c7b068ac118431b0e28f138d1e
jusmac <justyna.machalica@gmail.com>
2015-05-31 17:55:26 +0200

    accordion tla dla sekcji stopka

--- app.js
+++ app.js
@@ -149,8 +149,23 @@ var downloadList = function() {
     onSuccess(recipe);
 };
 
-var sliderSlice = function() {
-    $('#sb-slider').slicebox();
+var accordion = function() {
+    $('#accordion > li').hover(
+            function() {
+                var $this = $(this);
+                $this.stop().animate({'width': '480px'}, 500);
+                $('.heading', $this).stop(true, true).fadeOut();
+                $('.bgDescription', $this).stop(true, true).slideDown(500);
+                $('.description', $this).stop(true, true).fadeIn();
+            },
+            function() {
+                var $this = $(this);
+                $this.stop().animate({'width': '115px'}, 1000);
+                $('.heading', $this).stop(true, true).fadeIn();
+                $('.description', $this).stop(true, true).fadeOut(500);
+                $('.bgDescription', $this).stop(true, true).slideUp(700);
+            }
+    );
 };
 
 $(function() {
@@ -163,6 +178,7 @@ $(function() {
     addBorderWidthClone();
     addBorderHeightClone();
     downloadList();
+    accordion();
 });
 
 
