10007a924c10b86bad9437317e4422687d41ff43
jusmac <justyna.machalica@gmail.com>
2015-05-31 18:01:20 +0200

    poprawka w app.js

--- app.js
+++ app.js
@@ -61,24 +61,6 @@ var downloadRecipetoModalPage = function() {
     onSuccess(recipe);
 };
 
-var newSlider = function() {
-    $('.cd-testimonials-wrapper').flexslider({
-        //declare the slider items
-        selector: ".cd-testimonials > li",
-        animation: "slide",
-        //do not add navigation for paging control of each slide
-        controlNav: false,
-        slideshow: false,
-        //Allow height of the slider to animate smoothly in horizontal mode
-        smoothHeight: true,
-        start: function() {
-            $('.cd-testimonials').children('li').css({
-                'opacity': 1,
-                'position': 'relative'
-            });
-        }
-    });
-};
 
 var downloadRecipe = function() {
     recipe = {
@@ -172,7 +154,6 @@ $(function() {
     menu();
     downloadRecipetoSlider();
     downloadRecipetoModalPage();
-    newSlider();
     downloadRecipe();
     cdTestimonialsItemVisible();
     addBorderWidthClone();
